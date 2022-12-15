48a16b902ffe645ffdba5dc706a4a1fe9f2b7439
Aziz Gasimov <gasimovaziz@gmail.com>
2022-12-15 10:54:41 +0100

    initial commit

---
+++ csv-rest/src/main/java/com/luxoft/csvrest/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.luxoft.csvrest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping(value = "/")
+    public String get(){
+        return "IO API";
+    }
+}
